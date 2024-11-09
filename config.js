012fcd1fd7c383311a495306ecc0a08310c444b1
Naveen <vnaveenlgp2001@gmail.com>
2024-11-10 02:17:39 +0530

    check

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ module.exports = {
     MONGO_URI: `mongodb+srv://${process.env.USER_NAME}:${process.env.PASS_WORD}@cluster0.zbounx6.mongodb.net/?retryWrites=true&w=majority`,
     DATABASE_NAME: process.env.DATABASE_NAME,
     COLLECTION_NAME: process.env.COLLECTION_NAME,
-    ALLOWED_ORIGINS: ["http://localhost:3000","https://vooshfoods-frontend.vercel.app","https://vooshfoods-frontend-naveen4.vercel.app","https://vooshfoods-frontend-git-main-naveen4.vercel.app","https://todo-vooshfoods.vercel.app/"],
+    ALLOWED_ORIGINS: ["http://localhost:3000","https://vooshfoods-frontend.vercel.app","https://vooshfoods-frontend-naveen4.vercel.app","https://vooshfoods-frontend-git-main-naveen4.vercel.app","https://todo-vooshfoods.vercel.app","https://todo-vooshfoods-naveen4.vercel.app","https://todo-vooshfoods-git-main-naveen4.vercel.app"],
     JWT_SECRET: process.env.JWT_SECRET,
     JWT_EXPIRY: process.env.JWT_EXPIRY,
     GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
