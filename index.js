1ef9f1debac6124b301c59f659b5c5e9c79f8f12
Naveen <vnaveenlgp2001@gmail.com>
2024-11-13 13:18:55 +0530

    Google OAuth fixes

--- index.js
+++ index.js
@@ -38,7 +38,8 @@ passport.use(
     {
       clientID: GOOGLE_CLIENT_ID,
       clientSecret: GOOGLE_CLIENT_SECRET,
-      callbackURL: "/api/auth/google/callback",
+      callbackURL: "https://vooshfoods-backend.onrender.com/api/auth/google/callback",
+      // "/api/auth/google/callback",
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
